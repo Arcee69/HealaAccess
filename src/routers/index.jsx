21dd11287cbfd5caa160e82d603d3896ed7a7389
Shalom Mathew <shalommathew@Shaloms-MacBook-Pro.local>
2024-08-15 15:26:58 +0100

    update

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react';
-import { Routes, Route, useLocation} from 'react-router-dom';
-import Auth from '../pages/Auth';
-import Success from '../pages/Success';
-
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Auth from "../pages/Auth";
+import Success from "../pages/Success";
 
 const Routers = () => {
+  const { pathname } = useLocation();
 
-    const { pathname } = useLocation();
-  
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname]);
-    
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <Routes>
-      <Route exact path='/:id' element={<Auth />} />
-      <Route exact path='/success' element={<Success />} />
-    </Routes>
-  )
-}
+      <Route exact path="/" element={<Auth />} />
 
-export default Routers
+      <Route exact path="/:id" element={<Auth />} />
+      <Route exact path="/success" element={<Success />} />
+    </Routes>
+  );
+};
+
+export default Routers;
